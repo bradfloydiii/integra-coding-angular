bc12772729cfae3c26ac08551989eddcd83dfb08
Developer <developer@Developers-MacBook-Air.local>
2024-07-08 21:55:57 -0400

    first commit with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserDeleteComponent } from './components/user-delete/user-delete.component';
+
+const routes: Routes = [
+  {path: 'user/list',  component: UserViewComponent},
+  {path: 'user/create', component: UserCreateComponent},
+  {path: 'user/update', component: UserUpdateComponent},
+  {path: 'user/delete', component: UserDeleteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersService } from './services/users.service';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserCreateComponent } from './components/user-create/user-create.component';
+import { UserUpdateComponent } from './components/user-update/user-update.component';
+import { UserDeleteComponent } from './components/user-delete/user-delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent,
+    UserCreateComponent,
+    UserUpdateComponent,
+    UserDeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
