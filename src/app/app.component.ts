364789a47fc1f7cf54490cc2be1c5f5a69e30b74
Bradley Floyd III <bfloydiii@protonmail.com>
2024-07-11 16:44:41 -0400

    adding comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,35 @@ import { Router } from '@angular/router';
 import copydeck from 'src/assets/properties/properties';
 import { environment } from 'src/environments/environment';
 
+/**
+ * This is the root component of the application.
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  /**
+   * The title of the user view.
+   */
   public title = copydeck.responses.userView;
 
+  /**
+   * The title of the user view.
+   */
   userViewTitle = copydeck.titles.userView;
+
+  /**
+   * The title of the user create view.
+   */
   createUserTitle = copydeck.titles.userCreate;
 
   constructor(private router: Router) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of the component are initialized.
+   */
   ngOnInit() {
     if (environment.production) {
       console.log('We are running in production mode');
@@ -26,6 +42,9 @@ export class AppComponent {
     }
   }
 
+  /**
+   * Navigates to the user create page.
+   */
   createUser(): void {
     this.router.navigate(['/user/create']);
   }
--- src/app/components/user-view/user-view.component.ts
+++ src/app/components/user-view/user-view.component.ts
@@ -5,36 +5,79 @@ import { UsersService } from 'src/app/services/users.service';
 import copydeck from 'src/assets/properties/properties';
 
 
+/**
+ * UserViewComponent is responsible for displaying and managing user data.
+ */
 @Component({
   selector: 'user-view',
   templateUrl: './user-view.component.html',
   styleUrls: ['./user-view.component.css'],
 })
 export class UserViewComponent {
+  /**
+   * Array of users.
+   */
   users: IUsers[] = [];
+
+  /**
+   * Flag indicating if an error occurred.
+   */
   isError = false;
+
+  /**
+   * Error message to display.
+   */
   errorMessage = '';
 
+  /**
+   * Title for the update user action.
+   */
   updateUserTitle = copydeck.titles.userUpdate;
+
+  /**
+   * Title for the delete user action.
+   */
   deleteUserTitle = copydeck.titles.userDelete;
 
+  /**
+   * Creates an instance of UserViewComponent.
+   * @param userService The user service.
+   * @param router The router service.
+   */
   constructor(private userService: UsersService, private router: Router) {}
 
+  /**
+   * Lifecycle hook that is called after data-bound properties of a directive are initialized.
+   */
   ngOnInit(): void {
     this.userService.getUsers().subscribe({
-      next: (users) => this.users = users,
-      error: () => {this.isError = true; this.errorMessage = copydeck.responses.databaseError},
+      next: (users) => (this.users = users),
+      error: () => {
+        this.isError = true;
+        this.errorMessage = copydeck.responses.databaseError;
+      },
     });
   }
 
+  /**
+   * Updates the user.
+   * @param user The user to update.
+   */
   updateUser(user: IUsers): void {
     this.router.navigate(['/user/create', { user: JSON.stringify(user) }]);
   }
 
+  /**
+   * Deletes the user.
+   * @param id The ID of the user to delete.
+   */
   deleteUser(id: string): void {
     this.userService.deleteUser(id).subscribe({
-      error: () => {this.isError = true; this.errorMessage = copydeck.responses.userDeletedError},
-      complete: () => this.users = this.users.filter((user) => user.id !== id)
+      error: () => {
+        this.isError = true;
+        this.errorMessage = copydeck.responses.userDeletedError;
+      },
+      complete: () => (this.users = this.users.filter((user) => user.id !== id)),
     });
   }
 }
