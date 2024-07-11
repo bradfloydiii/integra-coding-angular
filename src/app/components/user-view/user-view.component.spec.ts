9fd7b13cee0677000145d66d2ad68a01c495a028
Bradley Floyd III <bfloydiii@protonmail.com>
2024-07-11 15:56:39 -0400

    fixes user-view test

--- src/app/components/user-view/user-view.component.spec.ts
+++ src/app/components/user-view/user-view.component.spec.ts
@@ -1,21 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserViewComponent } from './user-view.component';
 
-xdescribe('UserViewComponent', () => {
+describe('UserViewComponent', () => {
   let component: UserViewComponent;
-  let fixture: ComponentFixture<UserViewComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserViewComponent]
-    });
-    fixture = TestBed.createComponent(UserViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  const users = [
+    {
+      id: '1',
+      firstname: 'Mcmahon',
+      lastname: 'Fulton',
+      email: 'mcmahonfulton@illumity.com',
+      company: 'ILLUMITY',
+      phone: '+1 (814) 489-3373',
+    },
+  ];
+
+  const mockContactService = jasmine.createSpyObj('UserService', {
+    http: {},
+    getUsers: {
+      subscribe: () => {
+        return users;
+      },
+    },
+    createUser: {
+      subscribe: () => {
+        return users;
+      },
+    },
+    updateUser: {
+      subscribe: () => {
+        return users;
+      },
+    },
+    deleteUser: {
+      subscribe: () => {
+        return users;
+      },
+    },
   });
 
-  xit('should create', () => {
+  const mockRouter = jasmine.createSpyObj('Router', {
+    navigate: (dest: string) => {},
+  });
+
+  beforeEach(() => {
+    component = new UserViewComponent(
+      mockContactService,
+      mockRouter
+    );
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
